b50b3c0055ca778f6b6f5c689e549606f5a43048
natuchasca <natuxalee@gmail.com>
2016-06-30 13:20:21 -0400

    se agrega texto. Falta agregar checkbox y delete boton, y dejar fijo head. Dar estilo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
-function agregarTarea () {
+/* Debo tomar los datos que se ingresen en el textarea
+y enviarlos a un div con texto en la parte inferior de 
+la pagina al apretar el boton enviar
+y evitar que se envie tarea en blanco (valor) */
+
+
+document.getElementById("boton").addEventListener("click",
+	function () {
 	var box = document.getElementById("tareas");
-	var nuevo = document.createElement("div");
-	var tarea = document.getElementById("tarea");
-	nuevo.appendChild(tarea);
-	tareas.appendChild(nuevo);
-}
+	nueva = document.createElement("div");
+	nueva.setAttribute("class", "cajastarea");
+
+	var input = document.getElementById("inputtarea");
+	var tarea = input.value;
+	contenido = document.createTextNode(tarea);
+
+		if (tarea == "") {
+			alert("Ingrese tarea a realizar");
+		}
+		else {
+			nueva.appendChild(contenido);
+			box.appendChild(nueva);
+		}
+	}
+);
+
+
+
+
+/*
+
+	var trash = document.createElement("i");
+	trash.setAttribute("class", "fa fa-trash");
+	trash.addEventListener("click", 
+		function (){
+			box.innerHTML = "";
+		}
+	);
+
+	var checkbox = document.createElement("input");
+	checkbox.setAttribute("type", "checkbox");
+	checkbox.appendChild(box);
+
+*/
